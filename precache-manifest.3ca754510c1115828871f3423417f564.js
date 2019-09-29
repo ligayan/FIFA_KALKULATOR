c33a7f111192468cbc96ead29504270b37df895e
ligayan <daniel.nowosad89@gmail.com>
2019-09-29 19:14:47 +0200

    Updates

---
+++ precache-manifest.3ca754510c1115828871f3423417f564.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7607707427702c3c3ac9bd0f7cc4e1d",
+    "url": "/FIFA_KALKULATOR/index.html"
+  },
+  {
+    "revision": "c55a737e4b3a9034190d",
+    "url": "/FIFA_KALKULATOR/static/css/main.729573c3.chunk.css"
+  },
+  {
+    "revision": "edd757b5eab718a3a63f",
+    "url": "/FIFA_KALKULATOR/static/js/2.33367fcc.chunk.js"
+  },
+  {
+    "revision": "c55a737e4b3a9034190d",
+    "url": "/FIFA_KALKULATOR/static/js/main.21060e3b.chunk.js"
+  },
+  {
+    "revision": "6d6943e4c4ed79908fbd",
+    "url": "/FIFA_KALKULATOR/static/js/runtime-main.05f46631.js"
+  }
+]);
